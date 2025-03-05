821fa53d1051456ca3ae398b0842ea1643a8a152
Anbia Nuansyah <102801779+anbianuansyah@users.noreply.github.com>
2025-03-05 14:41:44 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,30 @@
 // create web server
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const qs = require('querystring');
-const comments = [];
-// create web server
-http.createServer((req, res) => {
-  const pathname = url.parse(req.url).pathname;
-  if (pathname === '/') {
-    fs.readFile(path.resolve(__dirname, 'index.html'), (err, data) => {
-      if (err) {
-        throw err;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  } else if (pathname === '/comment') {
-    if (req.method === 'POST') {
-      let body = '';
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-      req.on('end', () => {
-        const query = qs.parse(body);
-        comments.push(query);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(JSON.stringify(comments));
-      });
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
-}).listen(3000, () => {
-  console.log('Server is running...');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// create file
+fs.writeFileSync('comments.json', '[]');
+
+// get comments
+app.get('/comments', (req, res) => {
+  const comments = JSON.parse(fs.readFileSync('comments.json', 'utf-8'));
+  res.json(comments);
+});
+
+// add comment
+app.post('/comments', (req, res) => {
+  const comments = JSON.parse(fs.readFileSync('comments.json', 'utf-8'));
+  const comment = req.body.comment;
+  comments.push(comment);
+  fs.writeFileSync('comments.json', JSON.stringify(comments));
+  res.json({status: 'success'});
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
